5d804680552a86946ac3f2b48abf9105210e8333
Максим Богатырёв <bogatir202068@mail.ru>
2021-02-15 19:50:00 +0700

    Create procedure PrintFigures()

--- main.cpp
+++ main.cpp
@@ -13,8 +13,22 @@ void PrintEmptyLine()
     cout << endl;
 }
 
+void PrintFigures(int y)
+{
+    if (y == 0)
+    {
+        cout << "P  P  P  P  P  P  P  P";
+    }
+    else
+    {
+        cout << "R  Kt B  Q  K  B  Kt R";
+    }
+    cout << endl;
+}
+
 int main()
 {
     vector <string> figures = {"P", "Kt", "B", "R", "Q", "K"};
     return 0;
 }
+
