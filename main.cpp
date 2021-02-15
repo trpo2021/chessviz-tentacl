cf3d1cbe0d1298a61f51d84856ba4be851fc70bc
Максим Богатырёв <bogatir202068@mail.ru>
2021-02-15 19:51:18 +0700

    Add board output

--- main.cpp
+++ main.cpp
@@ -29,6 +29,14 @@ void PrintFigures(int y)
 int main()
 {
     vector <string> figures = {"P", "Kt", "B", "R", "Q", "K"};
+    PrintFigures(1);
+    PrintFigures(0);
+    for (int i=0; i<4; i++)
+    {
+        PrintEmptyLine();
+    }
+    PrintFigures(0);
+    PrintFigures(1);
     return 0;
 }
 
