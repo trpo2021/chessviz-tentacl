7e107f9b2f907d60bfae4e60b9ecff623d11035e
Максим Богатырёв <bogatir202068@mail.ru>
2021-02-15 19:31:20 +0700

    Create procedure PrintEmptyLine()

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+void PrintEmptyLine()
+{
+    for (int i=0; i<8; i++)
+    {
+        cout << "*  ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     vector <string> figures = {"P", "Kt", "B", "R", "Q", "K"};
